3688f859f625d6f3ea6228c9c216863903a2fd9f
Hamza <hamzamoha123@gmail.com>
2022-06-02 10:36:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
-test hello
-by hamza
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRMEF SM</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            text-decoration: none;
+        }
+        img.logo {
+            margin: 10px auto;
+            display: block;
+        }
+    </style>
+</head>
+<body>
+    <img class="logo" src="http://crmefsm.ac.ma/wp-content/uploads/2016/07/logo_crmef_sm.png" alt="CRMEF SM">
+    <h1>
+        Filiére Informatique
+    </h1>
+</body>
+</html>
