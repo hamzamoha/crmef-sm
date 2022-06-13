c74283dcadac825194b0592461cff80cc527dced
Hamza <hamzamoha123@gmail.com>
2022-06-13 15:06:08 +0100

    commit

---
+++ teacher-interface/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <div class="login">
+        <div class="login-box">
+            <img src="" alt="" class="login-icon">
+            <h1 class="login-title">Login</h1>
+            <form method="POST" class="login-form">
+                <input type="text" id="username" name="username" placeholder="Username">
+                <input type="password" id="password" name="password" placeholder="Password">
+                <input type="submit" value="Login" name="login" id="login">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
